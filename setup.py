6e2f22d4e710e90100387f62715282c58612e102
Clément Schreiner <clement@mux.me>
2014-06-19 23:34:59 +0200

    Add install_requires.

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup
 
 long_description = open('README.md', 'r').read()
 
+with open('requirements.txt') as f:
+    install_requires = [l for l in f.read().splitlines()
+                        if not l.startswith('#')]
+
+
 setup(
     name="loofah",
     version="0.1",
@@ -14,6 +19,7 @@ setup(
     author_email="paultag@debian.org",
     long_description=long_description,
     description='does some stuff with things & stuff',
+    install_requires=install_requires,
     license="Expat",
     url="",
     platforms=['any']
